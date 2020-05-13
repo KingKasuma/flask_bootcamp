980158a079671aa55400ab610f771999cb7fb334
Mike <lanselot777hailander@gmail.com>
2020-05-13 17:38:53 -0400

    loops

---
+++ python_course/loops.py
@@ -0,0 +1,14 @@
+seq = [1,2,3,4,5]
+
+print(1 in seq)
+
+for item in seq:
+    print(item)
+
+for x in range(0,11,2):
+    print(x)
+
+i = 1
+while i<5:
+    print("i is currently: {}".format(i))
+    i = i + 1
