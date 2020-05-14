f8e00c6d7593605b4f067eeae47fa02f1f3f98d9
Mike <lanselot777hailander@gmail.com>
2020-05-14 09:25:23 -0400

    flask basics

--- flask_course/flask_basics/basic.py
+++ flask_course/flask_basics/basic.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #127.0.0.1:5000
 def index():
     return "<h1>Hello Puppy!</h1>"
 
+@app.route('/information') #127.0.0.1:5000/information
+def info():
+    return "<h1>Puppies are cute!</h1>"
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return "100th letter: {}".format(name[100])
+
+@app.route('/puppy_name/<name>')
+def puppylatin(name):
+    pupname = ''
+    if name[-1] == 'y':
+        pupname = name[:-1] + 'iful'
+    else:
+        pupname = name + 'y'
+    return "<h1>Your puppy latin name is: {}</h1>".format(pupname)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
